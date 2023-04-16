5b871cf6a51bde567fb22b3533071a8eba0ae3fc
RupeshKarle <rupeshkarle.gen@gmail.com>
2023-04-16 21:23:13 +0530

    added one api route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     //
     public function passData($id) {
         return view("menu", ['id'=>$id]);
+    }
+    public function getData() {
+        return User::all();
     }
 }
